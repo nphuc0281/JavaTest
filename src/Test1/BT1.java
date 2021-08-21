df80bb3d4c8f886b9eafe09ae0508b8b3082fe70
nphuc0281 <nphuc0281@gmail.com>
2021-08-21 08:51:28 +0700

    clean code

--- src/Test1/BT1.java
+++ src/Test1/BT1.java
@@ -1,6 +1,5 @@
 package Test1;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
--- src/Test1/BT2.java
+++ src/Test1/BT2.java
@@ -9,142 +9,176 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class BT2 {
+	private static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+	private static String dbURL = "jdbc:sqlserver://103.95.197.121:9696;databaseName=JavaTest7";
+	private static String dbUser = "DACNPM";
+	private static String dbPass = "khoa19@itf";
 	private static Connection con;
+	
 	public static void main(String[] args) {
+		String url1 = "C:\\Users\\nphuc\\Desktop\\data1.txt";
+	    String url2 = "C:\\Users\\nphuc\\Desktop\\data2.txt";
+	    
+	    String[] data1 = null;
+	    String[] data2 = null;
 		try {
-			connectDB();
-		
-			String url1 = "C:\\Users\\nphuc\\Desktop\\data1.txt";
-		    String url2 = "C:\\Users\\nphuc\\Desktop\\data2.txt";
-		    
-		    InsertData(url1, 1);
-		    InsertData(url2, 2);
-	    
-	    
-			con.close();
-			System.out.println("Disconnected database!");
-		} catch (SQLException e) {
+			data1 = readData(url1);
+			data2 = readData(url2);
+		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
-	}
-	
-	private static void connectDB() {
-		try {
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			
-		    String dbURL = "jdbc:sqlserver://103.95.197.121:9696;databaseName=JavaTest7";
-		    String dbUser = "DACNPM";
-		    String dbPass = "khoa19@itf";
-		    
-		    con = DriverManager.getConnection(dbURL,dbUser,dbPass);
-		    System.out.println("Connected to database!");
-		    
+	    
+	    
+	    System.out.println("Content of data1.txt:");
+	    for(String i:data1) {
+	    	System.out.println(i);
+	    }
+	    
+	    System.out.println("\nContent of data2.txt:");
+	    for(String i:data2) {
+	    	System.out.println(i);
+	    }
+	    
+	    try {
+			insertData(url1, true);
 		} catch (Exception e) {
-			System.out.println("Err: " + e.getMessage());
+			System.out.println(e.getMessage());
 		}
-	}
-	
-	private static void InsertData(String url, int key) {
-		FileInputStream fileInputStream = null;
-        BufferedReader bufferedReader = null;
-        
-        try {
-            fileInputStream = new FileInputStream(url);
-            bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
-            
-            if(key == 1) {
-            	String line = bufferedReader.readLine();
-            	
-                while (line != null) {
-                	String[] sline = line.split(", ");
-                	
-                	for (int i = 0; i < sline.length; i++) {
-                		sline[i] = "'" + sline[i] +"'";
-                	}
-                	
-                	line = String.join(", ", sline);
-                	
-                	System.out.println(line);
-                	
-                	String query = "INSERT INTO THANHVIEN (MaThanhVien, TenThanhVien, NgaySinh, DiaChi, Email, SoDienThoai, ChiPhiNhan) VALUES ";
-                	
-                    excuteUpdate(query + "(" + line + ", 0)");
-                    
-                    line = bufferedReader.readLine();
-                }
-            } else if (key == 2) {
-            	String errurl = "C:\\Users\\nphuc\\Desktop\\error.txt";
-            	FileOutputStream fos = new FileOutputStream(errurl);
-            	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
-            	
-            	String line = bufferedReader.readLine();
-            	
-            	int index = 1;
-            	while (line != null) {
-            		String[] sline = line.split(", ");
-            		String error = checkLineError(sline, index);
-            		
-            		if (error == "") {
-            			int newCPN = 0;
-                		int diemthuong = 0;
-                		if (sline[2] == "VIP") diemthuong = Integer.parseInt(sline[1]) *50000;
-                		else if (sline[2] == "NOR") diemthuong = Integer.parseInt(sline[1])*20000;
-                		newCPN = Integer.parseInt(sline[1]) + diemthuong;
-                    	String query = "UPDATE THANHVIEN SET ChiPhiNhan = ChiPhiNhan + " + newCPN + " WHERE MaThanhVien = '" + sline[0] + "'";
-                    	                    	
-                        excuteUpdate(query);
-                        
-            		} else {
-            			bw.write(error);
-            		}
-            		
-            		line = bufferedReader.readLine();
-                    index++;
-                }
-            	
-            	bw.close();
-            	fos.close();
-            }
-            
-        } catch (Exception ex) {
-           System.out.println(ex.getMessage());
-        } finally {
-            try {
-                bufferedReader.close();
-                fileInputStream.close();
-            } catch (IOException ex) {
-                
-            }
-        }
-	}
-	
-	private static void excuteUpdate(String query) {
-        try {
-        	Statement stmt = con.createStatement();
-        	stmt.executeUpdate(query);
-		} catch (SQLException e) {
+	    try {
+			insertData(url2, false);
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
 	
-	private static String checkLineError(String[] line, int index) {
+	private static Connection getConnection() throws SQLException, ClassNotFoundException{
+			Class.forName(driver);
+		    return DriverManager.getConnection(dbURL,dbUser,dbPass);		    
+	}
+	
+	private static String[] readData(String url) throws IOException {
+		FileInputStream fileInputStream = new FileInputStream(url);
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+        
+        String res = "";
+		String line = bufferedReader.readLine();
+    	
+        while (line != null) {
+        	res += line + "\n";
+            line = bufferedReader.readLine();
+        }
+        
+        bufferedReader.close();
+        fileInputStream.close();
+        
+        return res.split("\n");
+	}
+	
+	private static void writeData(String url, String data) throws IOException {
+		FileOutputStream fileOutputStream = new FileOutputStream(url);
+    	BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(fileOutputStream));
+    	
+    	bufferedWriter.write(data);
+    	
+    	bufferedWriter.close();
+    	fileOutputStream.close();
+    	
+    	System.out.println("Data exported to " + url);
+	}
+	
+	private static void insertData(String url, Boolean mode) throws ClassNotFoundException, SQLException, IOException {
+		Connection con = getConnection();
+		PreparedStatement stmt;
+		
+		if (mode) {
+			String[] data = readData(url);
+			
+			
+			String query = "INSERT INTO THANHVIEN (MaThanhVien, TenThanhVien, NgaySinh, DiaChi, Email, SoDienThoai, ChiPhiNhan) VALUES (?,?,?,?,?,?,0)";
+			stmt = con.prepareStatement(query);
+			
+			for(String line : data) {
+				String[] parts = line.split(", ");
+				
+				stmt.setString(1, parts[0]);
+				stmt.setString(2, parts[1]);
+				stmt.setString(3, parts[2]);
+				stmt.setString(4, parts[3]);
+				stmt.setString(5, parts[4]);
+				stmt.setString(6, parts[5]);
+				
+				try {
+					stmt.executeUpdate();
+				} catch (SQLException e) {
+					System.out.println(e.getMessage());
+				}
+				
+			}
+			
+			stmt.close();
+			con.close();
+		} else {
+			String errurl = "C:\\Users\\nphuc\\Desktop\\error.txt";
+			String res = "";
+			String[] data = readData(url);
+			
+			String query = "UPDATE THANHVIEN SET ChiPhiNhan = ChiPhiNhan + ? where MaThanhVien = ?";
+			stmt = con.prepareStatement(query);
+			
+			int index = 1;
+			for(String line : data) {				
+				String[] parts = line.split(", ");
+				String error = checkLineError(parts, index);
+				
+				if (error == "") {
+            		int diemthuong = 0;
+            		if (parts[2] == "VIP") diemthuong = Integer.parseInt(parts[1]) *50000;
+            		else if (parts[2] == "NOR") diemthuong = Integer.parseInt(parts[1])*20000;
+            		int newCPN = Integer.parseInt(parts[1]) + diemthuong;
+                	                    	
+                    stmt.setInt(1, newCPN);
+                    stmt.setString(2, parts[0]);
+                    try {
+						stmt.executeUpdate();
+					} catch (SQLException e) {
+						System.out.println(e.getMessage());
+					}
+        		} else {
+        			res += error;
+        		}
+				
+				
+				index++;
+			}
+			
+			writeData(errurl, res);
+		}
+		
+		stmt.close();
+		con.close();
+		
+		System.out.println("Insert successfully! url = " + url);
+	}
+	
+	
+	private static String checkLineError(String[] line, int index) throws ClassNotFoundException, SQLException {
 		String res = "";
 		String query = "select * from THANHVIEN Where MaThanhVien = '" + line[0] +"'";
 		Statement stmt;
 		ResultSet set;
-		try {
-			stmt = con.createStatement();
-			set = stmt.executeQuery(query);
-			if (!set.next()) res += "Dong " + index + ": Khong tim thay ma thanh vien tuong ung\n";
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			System.out.println(e1.getMessage());
-		}
+		
+		Connection con = getConnection();
+		stmt = con.createStatement();
+		set = stmt.executeQuery(query);
+		if (!set.next()) res += "Dong " + index + ": Khong tim thay ma thanh vien tuong ung\n";
+		stmt.close();
+    	con.close();
     	
 		int i;
 		try {
